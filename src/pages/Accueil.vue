301107ad5cfe9e490ff88c664ed38a206e99cd3c
nihylin <nihylin@gmail.com>
2022-10-20 09:58:30 +0200

    ajout selfie + style bloc présentation

--- src/pages/Accueil.vue
+++ src/pages/Accueil.vue
@@ -1,19 +1,22 @@
 <template>
-    
-    <div>
-        <h2>Nos incontournables à faire et à voir</h2>
-        <p> Aujourd'hui nous t'emmenons chez nous : à Lille ! Et oui, c'est là que nous vivons depuis quelques années, tout
-            au Nord dans la magnifique capitale des Flandres. Et nous avons décidé de te faire découvrir les incontournables à faire
-            et à voir si tu t'y installe ou le temps d'un week-end. Entre nature, monuments ou encore gastronomie tu trouveras à coup sûr
-            ton bonheur dans ces articles pour un séjour en famille ou entre amis. </p>
-        <p>Bienvenue dans les Hauts-de-France !</p>
+
+    <div class="presentation">
+        <div>
+            <h2>Nos incontournables à faire et à voir</h2>
+            <p> Aujourd'hui nous t'emmenons chez nous : à Lille ! Et oui, c'est là que nous vivons depuis quelques années, tout au Nord dans la magnifique capitale des Flandres. Et nous avons décidé de te faire découvrir les incontournables à faire et à voir si tu t'y installes ou le temps d'un week-end.</p>
+            <p>Entre nature, monuments ou encore gastronomie tu trouveras à coup sûr ton bonheur dans ces articles pour un séjour en famille ou entre amis.</p>
+            <p>Bienvenue dans les Hauts-de-France !</p>
+        </div>
+        <div>
+            <img src="./../assets/img/selfie.jpg" alt="">
+        </div>
     </div>
 
     <div class="search">
         <input v-model="search" @input="getFilter()" placeholder="Rechercher par tag" />
     </div>
 
-   
+
     <div v-if="articles" class="articles">
         <article v-for="a of articles.slice(0, currentArticles)" :key="a.id + 'a'">
             <router-link :to="{name:`article`, params:{id: a.id}}">
@@ -57,39 +60,39 @@ export default {
             if (this.search !== '') {
                 this.error = '';
                 axios.get(`${this.api}?tags_like=${this.param}`)
-                    .then(({ data }) => (data.sort((a,b)=>new Date(b.date) - new Date(a.date)), this.articles = data))
+                    .then(({ data }) => (data.sort((a, b) => new Date(b.date) - new Date(a.date)), this.articles = data))
                     .catch(err => {
                         this.articles = undefined;
                         this.error = `${err.response.status} : ${err.message}`
                     })
             } else {
                 this.getArticles();
-            } 
+            }
         },
 
         getArticles() {
             this.error = '';
             axios.get(`${this.api}`)
-                .then(({ data }) => (data.sort((a,b)=>new Date(b.date) - new Date(a.date)), this.articles = data))
+                .then(({ data }) => (data.sort((a, b) => new Date(b.date) - new Date(a.date)), this.articles = data))
                 .catch(err => {
                     this.articles = undefined;
                     this.error = `${err.response.status} : ${err.message}`
                 })
         },
-        formatDate(date){
+        formatDate(date) {
             return new Date(date).toLocaleString('fr');
         },
-        showMore(){
+        showMore() {
             this.currentArticles += 6
         }
     },
     mounted() {
         this.getArticles();
     },
-    props:{
+    props: {
         user: {
             type: Object,
-            default:()=>(undefined)
+            default: () => (undefined)
         }
     },
     computed: {
@@ -97,7 +100,7 @@ export default {
             return this.search.split(/[ ,]+/).join(',')
         }
     },
-    emits:['connexion', 'deconnexion', 'updatedUser']
+    emits: ['connexion', 'deconnexion', 'updatedUser']
 }
 </script>
 
@@ -138,11 +141,12 @@ article img {
     object-fit: cover;
 }
 
-.date{
+.date {
     font-style: italic;
-    display:block;
-    margin-top:-5px;
+    display: block;
+    margin-top: -5px;
 }
+
 .tags small {
     background-color: rgba(51, 51, 51, 0.2);
     border-radius: 10px;
@@ -186,7 +190,7 @@ article img {
 .search input {
     border-radius: 10px;
     font-family: 'Helvetica Neue';
-    padding:10px;
+    padding: 10px;
     border: none;
     outline: none;
     box-shadow: 0 3px 15px rgba(51, 51, 51, 0.2);
@@ -196,4 +200,47 @@ article img {
     outline: 1px var(--main-color) solid;
 }
 
+.presentation {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 80%;
+    max-width: 1150px;
+    height: fit-content;
+    min-width: 300px;
+    backdrop-filter: blur(20px);
+    padding: 20px;
+    box-shadow: 0 3px 15px rgba(51, 51, 51, 0.2);
+    border-radius: 10px;
+    margin: 20px;
+}
+
+.presentation p {
+    margin-bottom: 5px;
+}
+
+.presentation div {
+    max-width: 50%;
+    padding: 20px;
+}
+
+.presentation img {
+    max-height: 300px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+@media screen and (max-width:768px) {
+    .presentation {
+        flex-direction: column;
+
+    }
+
+    .presentation div {
+        max-width: unset;
+        width: 100%;
+    }
+
+}
 </style>
